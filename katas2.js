b145816f91df16fdbc538e9363f6d668cf3b89e1
Ryan <jragard@gmail.com>
2019-01-15 13:28:16 -0500

    testing

--- katas2.js
+++ katas2.js
@@ -10,44 +10,44 @@ function multiply(x, y) {
     return x;
 }
 
-function power(x, n) {
-    let total = x;
-    for(let counter = 1; counter < n; counter = add(counter, 1)) {
-        total = multiply(total, x)
-    }
-    return total;
-}
+// function power(x, n) {
+//     let total = x;
+//     for(let counter = 1; counter < n; counter = add(counter, 1)) {
+//         total = multiply(total, x)
+//     }
+//     return total;
+// }
 
-function factorial(x) {
-    let total = x;
-    let inc = total;
+// function factorial(x) {
+//     let total = x;
+//     let inc = total;
 
-    while(inc > 1) {
-        total = multiply(total, (inc - 1))
-        inc = inc - 1
-    }
-    return total;
-}
+//     while(inc > 1) {
+//         total = multiply(total, (inc - 1))
+//         inc = inc - 1
+//     }
+//     return total;
+// }
 
-function fibonacci(n) {
+// function fibonacci(n) {
 
-    let a = 0
-    let b = 1
-    let next;
+//     let a = 0
+//     let b = 1
+//     let next;
 
-    if(n == 1) {
-        return a;
-    }
-    else if(n == 2) {
-        return b;
-    }
-    else {
-        while(n >= 3) {
-            next = add(a,b);
-            a = b;
-            b = next;
-            n--;
-        }
-    }
-    return next;
-}
+//     if(n == 1) {
+//         return a;
+//     }
+//     else if(n == 2) {
+//         return b;
+//     }
+//     else {
+//         while(n >= 3) {
+//             next = add(a,b);
+//             a = b;
+//             b = next;
+//             n--;
+//         }
+//     }
+//     return next;
+// }
